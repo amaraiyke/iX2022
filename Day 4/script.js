a072e025bc0ad274a14aeb58c6a04d146e7f6181
amaraiyke <ama.iykeuzo@gmail.com>
2022-06-03 14:31:54 -0400

    added day 2, 3 and 4

---
+++ Day 4/script.js
@@ -0,0 +1,43 @@
+const movies = [
+    {title: 'Harry Potter', explanation: 'This movie is about a dude with a stick...', hint: 'It\'s Magic'},
+    {title: 'Just Go With It', explanation: 'This movie is about people who go on holiday...', hint: 'Adam, Drew and Jennifer'},
+    {title: 'Never Back Down', explanation: 'This movie is about two guys with daddy issues who beat each other up...', hint: 'Kanye West - Stronger'},
+    {title: 'Spongebob Squarepants', explanation: 'This movie is about a rectangle...', hint: 'It\'s a cartoon'},
+    {title: '50 First Dates', explanation: 'This movie is about a chick, she has the worst memory...', hint: '50 times...'},
+    {title: 'Cars', explanation: 'In this movie, car go fast...', hint: 'Kachow'},
+    {title: 'Spiderman', explanation: 'In this movie this guy just does not pay his rent, no matter how many times the landlord asks...', hint: 'Peta-Paka'},
+    {title: 'The Wolf Of Wall Street', explanation: 'In this movie there\'s like illegal stuff, lots of money, and a blonde chick...', hint: 'HAWOOooooooooooo'},
+    {title: 'Inception', explanation: 'In this movie everyone is like sleeping all the time...', hint: 'Dreams...'},
+    {title: 'Peter Pan', explanation: 'In this movie some kids die and an angel escorts them to heaven...', hint: 'Always flying, cuz he neverlands'},
+    {title: 'The Lord Of The Rings', explanation: 'In this movie some small guys go for a walk...', hint: 'You will not vacate past this exact position'}
+   ]
+   
+
+const form = document.getElementById('form');
+const movie = document.getElementById('movie.name');
+const guess = document.getElementById('guess');
+
+const random = Math.floor(Math.random() * movies.length);
+console.log(movies[random])
+
+form.addEventListener('submit'), (e) => {
+    e.preventDefault();
+}
+
+  // Check for our movie value
+  console.log(movie.value);
+
+function checkIfCorrect(movie) {
+  
+    console.log('Random: ', random);
+    console.log('Guess: ', movie);
+  
+    guess.classList.remove('hide');
+  
+    // Note, a === checks for type but here we are comparing a string and a number so we can just use a == for the check
+    if (movie === random) {
+      guess.innerHTML = 'Hey you got it!';
+    } else {
+      guess.innerHTML = 'Nope, try again lol';
+    }
+  }
